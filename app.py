3c7d01ddd0b28f48413ddc0b2539b4291f1a8914
Alana Zimmerman <azimmer4@terpmail.umd.edu>
2024-03-28 18:28:05 -0400

    Added the flask "request" to the top of the app.py. Added the favorite-course.html page to my file to print out the queries passed into the url bar on the page. Added the contact.html route with an if statement dictating what to display before and after the form is submitted on the page by using the input by the user.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,23 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('You entered for favorite course as: ' + request.args.get('subject') + request.args.get('course_number'))
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        print('First name entered: ' + request.form.get('first_name'))
+        print('Last name entered: ' + request.form.get('last_name'))
+        print('Email entered: ' + request.form.get('email'))
+        print('Phone Number entered: ' + request.form.get('phone_number'))
+        print('Preferred method of contact chose: ' + request.form.get('contact_method'))
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
